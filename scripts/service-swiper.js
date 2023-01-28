db3466311ac2c161310ed400cda96e1af6404d1f
pozchaa <igorleonsob@icloud.com>
2023-01-29 02:40:38 +0300

    фикс после рефью 2

--- scripts/service-swiper.js
+++ scripts/service-swiper.js
@@ -1,8 +1,12 @@
-const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 'auto',
-  spaceBetween: 16,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+let globalWidth = window.innerWidth;
+
+if (globalWidth <= 768) {
+  var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
--- scripts/service.js
+++ scripts/service.js
@@ -5,12 +5,16 @@ function changeServiceBrands() {
   if (!serviceBrands.classList.contains('service__brands_active')) {
     serviceMoreBtn.classList.add('service__brands-more_active');
     serviceBrands.classList.add('service__brands_active');
+    serviceMoreBtn.innerHTML = 'Скрыть';
   } else {
     serviceMoreBtn.classList.remove('service__brands-more_active');
     serviceBrands.classList.remove('service__brands_active');
+    serviceMoreBtn.innerHTML = 'Показать все';
   }
 }
 
 serviceMoreBtn.addEventListener('click', function () {
   changeServiceBrands();
 });
+
+console.log(serviceMoreBtn.value);
